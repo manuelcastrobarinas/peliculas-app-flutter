a1802e9347874d08bcfb9075db2c1de27bdb30f4
manuelcastrobarinas <manuel.castrobarinas@gmail.com>
2022-10-09 22:52:49 -0500

    add a provider model and service to get new movies now

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -70,25 +70,28 @@ class _PosterAndTitle extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(children: [
         ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: const FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'),
-            image: NetworkImage('https://loremflickr.com/640/360'),
+          borderRadius: BorderRadius.circular(40),
+          child:  FadeInImage(
+            placeholder: const AssetImage('assets/no-image.jpg'),
+            image: const NetworkImage('https://loremflickr.com/640/360'),
             height: 150,
+            width: MediaQuery.of(context).size.width * 0.4,
           ),
         ),
-        const SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('movie.title',style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2,),
-            Text('movie.originalTitle',style: textTheme.subtitle1, overflow: TextOverflow.ellipsis),
-            Row(children: [
-              const Icon(Icons.star_border_outlined,size: 15,color: Colors.grey),
-              const SizedBox(width: 5),
-              Text('movie.voteAverange', style: textTheme.caption),
-            ])
-        ]),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title',style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2,),
+              Text('movie.originalTitle',style: textTheme.subtitle1, overflow: TextOverflow.ellipsis),
+              Row(children: [
+                const Icon(Icons.star_border_outlined,size: 15,color: Colors.grey),
+                const SizedBox(width: 5),
+                Text('movie.vote', style: textTheme.caption, maxLines: 2, overflow:  TextOverflow.ellipsis),
+              ])
+          ]),
+        ),
       ]),
     );
   }
